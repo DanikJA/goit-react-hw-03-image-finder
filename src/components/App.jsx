56b5454d9ab4532e959f3a1e6cd307264cfcfad8
Danik2023 <danik.kyrteu2@gmail.com>
2025-01-17 18:00:23 +0200

    ...

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -77,7 +77,7 @@ export class App extends Component {
     const { images, loading, largeImageURL, showModal } = this.state;
     return (
       <>
-        <div className="mainDiv">
+        <div>
           <Searchbar onSubmit={this.handleSearchSubmit}></Searchbar>
           {loading && <Loader />}
           <ImageGallery
